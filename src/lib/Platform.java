883428e0895eae9000df680c5b36c88b9d54e09b
test <test@mail.riu>
2019-04-01 18:20:11 +0500

    01. Создание метода Platform и подключение skip

---
+++ src/lib/Platform.java
@@ -0,0 +1,71 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class Platform {
+
+    private static final String PLATFORM_IOS = "ios";
+    private static final String PLATFORM_ANDROID = "android";
+    private static final String Appium_URL = "http://127.0.0.1:4723/wd/hub";
+
+    public AppiumDriver getDriver() throws Exception
+    {
+        URL URL = new URL(Appium_URL);
+        if (this.isAndroid()) {
+            return new AndroidDriver(URL, this.getAndroidDesiredCapabilities());
+        } else if (this.isIOS()) {
+            return new IOSDriver(URL, this.getIOSDesiredCapabilitises());
+        } else {
+            throw new Exception("Некоректное имя платформы. Значение " + this.getPlatformVar());
+        }
+    }
+
+    public boolean isAndroid()
+    {
+        return isPlatform(PLATFORM_ANDROID);
+    }
+
+    public boolean isIOS()
+    {
+        return isPlatform(PLATFORM_IOS);
+    }
+
+    private DesiredCapabilities getAndroidDesiredCapabilities()
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","emulator-5554");
+        capabilities.setCapability("platformVersion","8.0");
+        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("app","/Users/Nastia/Documents/GitHub/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        return capabilities;
+    }
+
+    private DesiredCapabilities getIOSDesiredCapabilitises()
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName","IOS");
+        capabilities.setCapability("deviceName","iPhone 5E");
+        capabilities.setCapability("platformVersion","11.3");
+        capabilities.setCapability("app","/Users/Nastia/Desktop/JavaAppiumAutomation/apks/Wikipedia.app");
+        return capabilities;
+    }
+
+    private boolean isPlatform(String my_platform)
+    {
+        String platform = this.getPlatformVar();
+        return my_platform.equals(platform);
+    }
+
+    private String getPlatformVar()
+    {
+        return System.getenv("Platform");
+    }
+}
--- src/test/GetStartedTest.java
+++ src/test/GetStartedTest.java
@@ -1,14 +1,18 @@
-package test.IOS;
+package test;
 
-import lib.IOSTestCase;
+import lib.CoreTestCase;
 import lib.ui.WelcomePageObject;
 import org.junit.Test;
 
-public class GetStartedTest extends IOSTestCase {
+public class GetStartedTest extends CoreTestCase {
 
     @Test
     public void testPassThroughWelcome()
     {
+        if (this.Platform.isAndroid()) {
+            return;
+        }
+
         WelcomePageObject WelcomePage = new WelcomePageObject(driver);
 
         WelcomePage.waitForLearnMoreLink();
