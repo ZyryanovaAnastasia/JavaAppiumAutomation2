308494795cb1d0a0dd30b082a17ae46e989c294d
app.esoft.tech <app@esoft.tech>
2019-04-14 23:46:33 +0500

    ex5 for IOS

--- src/homework/EX5.java
+++ src/homework/EX5.java
@@ -1,12 +1,15 @@
 package homework;
 
 import lib.CoreTestCase;
+import lib.Platform;
 import lib.ui.ArticlePageObject;
 import lib.ui.MyListPageObject;
 import lib.ui.NavigationUi;
+import lib.ui.SearchPageObject;
 import lib.ui.factories.ArticlePageObjectFactory;
 import lib.ui.factories.MyListPageObjectFactory;
 import lib.ui.factories.NavigationUiFactory;
+import lib.ui.factories.SearchPageObjectFactory;
 import org.junit.Test;
 
 public class EX5 extends CoreTestCase {
@@ -15,36 +18,49 @@ public class EX5 extends CoreTestCase {
     public void testSaveAndDeleteTwoArticle()
     {
         String search_line = "Java";
-        String article_name_one = "JavaScript";
-        String article_name_two = "Java";
+        String article_name_one = "Island of Indonesia";
+        String article_name_two = "Programming language";
         String name_of_folder = "Learning programming";
 
+        SearchPageObject SearchPage = SearchPageObjectFactory.get(driver);
         ArticlePageObject ArticlePage = ArticlePageObjectFactory.get(driver);
         NavigationUi NavigationUi = NavigationUiFactory.get(driver);
         MyListPageObject MyListPage = MyListPageObjectFactory.get(driver);
 
-        ArticlePage.searchAndOpenArticle(search_line, article_name_one);
-        ArticlePage.addArticleToNewList(name_of_folder);
+        SearchPage.intSearchInput();
+        SearchPage.typeSearchLine(search_line);
+        SearchPage.clickByArticleWithSubstring(article_name_one);
+
+        ArticlePage.waitForTitleElement();
+        String article_title = ArticlePage.getArticleTitle();
+
+        if (Platform.getInstance().isAndroid()) {
+            ArticlePage.addArticleToNewList(name_of_folder);
+        } else {
+            ArticlePage.addArticlesToMySaved();
+        }
+
         ArticlePage.closeArticle();
 
-        ArticlePage.searchAndOpenArticle(search_line, article_name_two);
-        ArticlePage.addArticleToSpecialList(name_of_folder);
+        SearchPage.intSearchInput();
+        SearchPage.clickByArticleWithSubstring(article_name_two);
+
+        ArticlePage.waitForTitleElement();
+
+        if (Platform.getInstance().isAndroid()) {
+            ArticlePage.addArticleToNewList(name_of_folder);
+        } else {
+            ArticlePage.addArticlesToMySaved();
+        }
+
         ArticlePage.closeArticle();
 
         NavigationUi.clickMyLists();
-        MyListPage.openFolderByName(name_of_folder);
 
-        MyListPage.swipeByArticleToDelete(article_name_one);
-        MyListPage.waitForArticleToAppearByTitle(article_name_two);
+        if (Platform.getInstance().isAndroid()) {
+            MyListPage.openFolderByName(name_of_folder);
+        }
 
-        ArticlePage.clickArticleName(article_name_two);
-        String gotten_article_title = ArticlePage.getArticleTitle();
-
-        assertEquals(
-                "Ожидалось, что заголовок статьи будет " + article_name_two,
-                article_name_two,
-                gotten_article_title
-        );
-
+        MyListPage.swipeByArticleToDelete(article_title);
     }
 }
